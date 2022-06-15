91ed0a8e24172e3db32fd56bcd1f1d70acd23830
Vikramaditya Pedamallu <pedamalluv@Vikramadityas-MacBook-Pro.local>
2022-06-15 23:59:31 +0530

    initial commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,12 +1,24 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { ActionsComponent } from './actions/actions.component';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './header/header.component';
+import { HintComponent } from './hint/hint.component';
+import { HomeComponent } from './home/home.component';
+import { NoSlotComponent } from './no-slot/no-slot.component';
+import { TicketComponent } from './ticket/ticket.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ActionsComponent,
+    HeaderComponent,
+    HomeComponent,
+    TicketComponent,
+    NoSlotComponent,
+    HintComponent
   ],
   imports: [
     BrowserModule,
---
+++ src/app/no-slot/no-slot.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-no-slot',
+  templateUrl: './no-slot.component.html',
+  styleUrls: ['./no-slot.component.scss']
+})
+export class NoSlotComponent {
+  title = 'bingo';
+}
